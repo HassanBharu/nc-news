9ec852684cbb0eb7f6df30968bfe867c6bdc6140
HassanBharu <hassan_7_8_6@hotmail.com>
2019-06-18 18:00:47 +0100

    updated urls

--- db/data/dev-data/users.js
+++ db/data/dev-data/users.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         username: 'tickle122',
         name: 'Tom Tickle',
-        avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+        avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.jpg/revision/latest?cb=20180127221953',
     },
     {
         username: 'grumpy19',
@@ -17,12 +17,12 @@ module.exports = [
     {
         username: 'cooljmessy',
         name: 'Peter Messy',
-        avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+        avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/1/17/Mr._Messy_show.jpg/revision/latest/scale-to-width-down/180?cb=20180723214026',
     },
     {
         username: 'weegembump',
         name: 'Gemma Bump',
-        avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+        avatar_url: 'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.jpg/revision/latest/scale-to-width-down/250?cb=20180123225553',
     },
     {
         username: 'jessjelly',
